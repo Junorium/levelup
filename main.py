0359a5eb4a6e07056ade5e885915957f00555083
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-02-08 19:35:52 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def ranking_assignment(strength, stamina, intelligence):
+  # strength measurements
+  if strength[0]
+  if strength[1]
+  if strength[2]
+  # stamina measurements
+  # intelligence measurements
+
+ranks = 
+
+class Profile:
+  def __init__(self, rank, status, title):
+    for rank in ranks:
+      if rank == self.rank():
+        
